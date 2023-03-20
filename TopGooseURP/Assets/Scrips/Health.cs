d8aed6677ec2d968f9c2a980c23ce7359f475cd8
amFrendly <112705227+amFrendly@users.noreply.github.com>
2023-03-20 12:39:57 +0100

    Particles when Bullets hit stuff

---
+++ TopGooseURP/Assets/Scrips/Health.cs
@@ -0,0 +1,67 @@
+using System;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public delegate void OnDeadEvent();
+    OnDeadEvent OnDead;
+
+    public delegate void OnChangeHealthEvent(float change);
+    OnChangeHealthEvent OnChangeHealth;
+
+    [NonSerialized]
+    public float health;
+
+    [NonSerialized]
+    public bool dead;
+
+    public float startHealth;
+
+
+
+    public void AddDeathEvent(OnDeadEvent deadEvent)
+    {
+        OnDead += deadEvent;
+    }
+    public void RemoveDeathEvent(OnDeadEvent deadEvent)
+    {
+        OnDead -= deadEvent;
+    }
+    public void AddChangeHealthEvent(OnChangeHealthEvent changeHealthEvent)
+    {
+        OnChangeHealth += changeHealthEvent;
+    }
+    public void RemoveChangeHealthEvent(OnChangeHealthEvent changeHealthEvent)
+    {
+        OnChangeHealth -= changeHealthEvent;
+    }
+
+    public void ClearDeathEvents()
+    {
+        OnDead = null;
+    }
+
+    public void Refresh()
+    {
+        health = startHealth;
+        dead = false;
+    }
+
+    public void ChangeHealth(float change)
+    {
+        health += change;
+        OnChangeHealth?.Invoke(change);
+
+        if (health > startHealth) health = startHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health <= 0 && !dead)
+        {
+            OnDead?.Invoke();
+            dead = true;
+        }
+    }
+}
