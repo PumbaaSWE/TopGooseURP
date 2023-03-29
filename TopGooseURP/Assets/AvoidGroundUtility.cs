6ed10985145e4af9c5d2c685c8a817d2a6725be4
Winn1337 <112430462+Winn1337@users.noreply.github.com>
2023-03-29 16:08:32 +0200

    AvoidGroundUtility V0.4
    Not sure if we can call this an improved version, but it's another version. Pushing this so I can insert an awesome radio in separate branch

--- TopGooseURP/Assets/AvoidGroundUtility.cs
+++ TopGooseURP/Assets/AvoidGroundUtility.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using Random = System.Random;
 
 public class AvoidGroundUtility : MonoBehaviour, IUtility
 {
@@ -52,9 +53,35 @@ public class AvoidGroundUtility : MonoBehaviour, IUtility
 
     public void Execute()
     {
-        Vector3 newTarget = rb.position + Vector3.Cross(hit.normal, Vector3.Cross(rb.velocity, hit.normal)) * 2;
+        
 
+        Vector3 newTarget = rb.position + Vector3.Cross(hit.normal, Vector3.Cross(rb.velocity, hit.normal)) + hit.normal * 2;
         Debug.DrawLine(newTarget, rb.position, Color.green);
+
+        Random rand = new Random();
+
+        if (Physics.SphereCast(rb.position, sphereRadius, newTarget, out hit, Vector3.Distance(rb.position, newTarget)) || rand.Next(2) < 1)
+        {
+            Vector3[] potentialTargets = new Vector3[4];
+
+            potentialTargets[0] = rb.position + (rb.transform.forward * 2 + rb.transform.right) / 3 * rb.velocity.magnitude;
+            potentialTargets[1] = rb.position + (rb.transform.forward * 2 - rb.transform.right) / 3 * rb.velocity.magnitude;
+            potentialTargets[2] = rb.position + (rb.transform.forward * 2 + rb.transform.up) / 3 * rb.velocity.magnitude;
+            potentialTargets[3] = rb.position + (rb.transform.forward * 2 - rb.transform.up) / 3 * rb.velocity.magnitude;
+
+            for (int i = 0; i < potentialTargets.Length; i++)
+            {
+                Debug.DrawLine(potentialTargets[i], rb.position, Color.yellow);
+
+                // Räcker inte med raycast, testa spherecast och kanske overlapsphere ifall spherecastdistans är 0
+                if (Physics.Raycast(rb.position, potentialTargets[i], Vector3.Distance(rb.position, potentialTargets[i])))
+                    newTarget = potentialTargets[i];
+
+                Debug.Log($"Chose secondary target #{i}");
+            }
+        }
+        else
+            Debug.Log("Chose primary target");
 
         ap.RunAutopilot(newTarget, out float pitch, out float yaw, out float roll);
         fc.SetControlInput(new Vector3(pitch, yaw, roll));
