18d601807cd69d289e01a4a2b8fdebde20321712
PumbaaSWE <ballejalle@hotmail.com>
2023-04-23 00:26:10 +0200

    Updated Weapons

--- TopGooseURP/Assets/Scrips/WeaponS/Weapon.cs
+++ TopGooseURP/Assets/Scrips/WeaponS/Weapon.cs
@@ -1,17 +1,7 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public abstract class Weapon : MonoBehaviour
 {
+    protected TeamMember owner;
 
-    public bool IsActive { get;  protected set; }
-
-    internal abstract void Activate();
-
-    internal abstract void Deactivate();
-
-    internal abstract void Deploy();
 }
